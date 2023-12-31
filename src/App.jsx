650ba1c8c1cc34c2fed8bfeaed92cd0665473a95
Dhanush <k.varddhan2002@gmail.com>
2023-12-31 09:11:09 +0530

    added home again

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
-import React from 'react'
+import { BrowserRouter,Routes, Route } from 'react-router-dom'
 import './App.css'
 import Navbar from './components/Navbar'
 import Contact from './pages/contact/Contact'
 import Home from '/pages/home/Home'
+import About from './pages/about/About'
+import Portfolio from './pages/portfolio/Portfolio'
 function App() {
-  
-
   return (
-   <>
+   <BrowserRouter>
+   <Navbar/>
+     <Routes>
+      {/* <Route index element ={<Home/>} /> */}
+      <Route path='about' element={<About/>}/>
+      <Route path='portfolio' element={<Portfolio/>}/>
+      <Route path='contact' element={<Contact/>}/>
+      <Route path='about' element={<About/>}/>
+
+
+     </Routes>
    
-   
-   </>
-  )
+   </BrowserRouter>
+  );
 }
 
 export default App

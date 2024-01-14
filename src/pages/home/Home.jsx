7c7390973ba427a1f0984a398d5d80a910bdf6fa
Dhanush <k.varddhan2002@gmail.com>
2024-01-14 23:57:07 +0530

    developed home page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,12 +1,34 @@
 import React from 'react'
 import Profile from "../../assets/home.jpg"
-
-import { link } from 'react-router-dom'
+import { Link } from 'react-router-dom'; // Note the corrected import
+import { FaArrowRight } from "react-icons/fa";
+import "./home.css"
 const Home = () => {
   return (
     <div>
      <section className="home section grid">
-      
+       <img src={Profile} alt="" className='home__img' />
+       <div className="home__content">
+        <div className="home__data">
+          <h1 className="home__title">
+            <span>
+            I am Dhanush.  
+            </span>         
+              web designer  
+          </h1>
+          <p className="home__description">
+            I am focused in developing clean and neat website. I love developing 3d website.
+            bla bla........
+          </p>
+          <Link to='/about' className='button'>
+            More about me{' '}
+            <span className="button__icon">
+              <FaArrowRight/>
+            </span>
+
+          </Link>
+        </div>
+       </div>
      </section>
     </div>
   )

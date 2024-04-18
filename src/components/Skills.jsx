f83454631f1a5414cc5d3205db72aa5b3a3f18c2
Dhanush <k.varddhan2002@gmail.com>
2024-04-18 22:04:53 +0530

    successful tilt

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,51 +1,35 @@
-// import React from 'react'
-// import { skills } from '../data'
-// import { CircularProgressbar } from 'react-circular-progressbar';
-// import 'react-circular-progressbar/dist/styles.css';
-
-// const Skills = () => {
-//   return (
-//    <>
-//    {skills.map(({title,percentage,image},index)=>{
-//     return(
-//         <div className="progress__box" key={index}>
-//             <div>
-//               <img src={image} alt={title} style={{ width: 120, height: 120 }} />
-//               <h3 className="skills__title">{title}</h3>
-
-//             </div>
-            
-//         </div>
-//     )
-//    })}
-//    </>
-//   )
-// }
-
-// export default Skills
-
-import React from 'react';
-import { skills } from '../data';
-import Tilt from 'react-tilt';
-
+import React from 'react'
+import { skills } from '../data'
+import { Tilt } from "react-tilt";
+import 'react-circular-progressbar/dist/styles.css';
+import "./skills.css"
 const Skills = () => {
   return (
-    <>
-      {skills.map(({ title, percentage, image }, index) => {
-        return (
-          <div className="progress__box" key={index}>
-            <Tilt className="Tilt" options={{ max: 25, scale: 1.05 }}>
-              <div className="Tilt-inner">
-                <img src={image} alt={title} style={{ width: 75, height: 75 }} />
+   <>
+   
+   {skills.map(({title,image},index)=>{
+    return(
+        <div key={index}>
+            <div>
+                <Tilt>
+                <img src={image} alt={title} style={{ width: 120, height: 120, }} />
                 <h3 className="skills__title">{title}</h3>
-              </div>
-            </Tilt>
-          </div>
-        );
-      })}
-    </>
-  );
-};
+                </Tilt>
+                
+                
+                
+              
+              
 
-export default Skills;
+            </div>
+            
+        </div>
+    )
+   })}
+   </>
+  )
+}
+
+export default Skills
+
 

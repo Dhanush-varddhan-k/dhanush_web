331dd2fb36fa0628ff614a44f5cd11ea8cb86e39
Dhanush <k.varddhan2002@gmail.com>
2024-04-19 11:12:13 +0530

    final3

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -1,15 +1,15 @@
-import React from 'react'
-import{
-  FaEnvelopeOpen,
-  FaPhoneSquareAlt,
-  FaLinkedin,
-  
 
-} from 'react-icons/fa';
 
-import { FiSend } from 'react-icons/fi'
-import "./contact.css"
+import React from 'react';
+import { FaEnvelopeOpen, FaPhoneSquareAlt, FaLinkedin } from 'react-icons/fa';
+import { FiSend } from 'react-icons/fi';
+import "./contact.css";
+
 const Contact = () => {
+  const handleMailClick = () => {
+    window.location.href = 'mailto:dhanush21110456@snuchennai.edu.in';
+  };
+
   return (
     <section className="contact section">
       <h2 className="section__title">
@@ -21,11 +21,9 @@ const Contact = () => {
           <h3 className="contact__title">
             Wan'na hire me
           </h3>
-          <p className="contact__decription">
-            Open to discuss new topics and different project ideas.
-          </p>
+          <p className="contact__decription"></p>
           <div className="contact__info">
-            <div className="info__item">
+            <div className="info__item" onClick={handleMailClick}>
               <FaEnvelopeOpen className='info__icon'/>
               <div>
                 <span className="info__title">Mail me</span>
@@ -40,45 +38,17 @@ const Contact = () => {
                 <h4 className="info__desc">+91 6381562738</h4>
               </div>
             </div>
-
-
-
-
           </div>
 
           <div className="contact__socials">
             <a href="https://www.linkedin.com/in/dhanush-varddhan-712594249/" className="contact__social-link">
-            <FaLinkedin />
+              <FaLinkedin />
             </a>
           </div>
         </div>
-        {/* <form className="contact__form">
-          <div className="form__input-group">
-            <div className="form__input-div">
-              <input type="text" placeholder='Your Name' className="from__control" />  
-            </div>
-            <div className="form__input-div">
-              <input type="email" placeholder='Your Email' className="from__control" />
-            </div>  
-            <div className="form__input-div">
-              <input type="text" placeholder='Subject' className="from__control" />
-            </div>          
-          </div>
-          <div className="form__input-div">
-              <textarea placeholder='Your Message' className="form__control textarea">
-              </textarea>
-            </div>
-             <button className="button">
-               Send Message
-               <span className="button__icon contact__button-icon">
-                 <FiSend/>
-               </span>
-            </button>
-        </form> */}
       </div>
-
     </section>
-  )
-}
+  );
+};
 
-export default Contact
+export default Contact;
